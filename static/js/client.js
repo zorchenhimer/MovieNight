1b8dd76a45cb2057a96f01e88f54124fc1c06fa8
joeyak <joeyak97@gmail.com>
2019-03-16 17:59:10 -0400

    Add help button

--- static/js/client.js
+++ static/js/client.js
@@ -134,6 +134,10 @@ function auth() {
     }
 }
 
+function help() {
+    sendMessage("/help")
+}
+
 // Make this on all pages so video page also doesn't do this
 $(document).on("keydown", function (e) {
     if (e.which === 8 && !$(e.target).is("input, textarea")) {
