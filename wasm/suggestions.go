24b5152c38671a43cce934692d708d85aed9d03d
joeyak <joeyak97@gmail.com>
2019-03-25 23:32:44 -0400

    Moving suggestion code to a separate file in prep for issue #9

---
+++ wasm/suggestions.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/dennwc/dom/js"
+)
+
+const (
+	keyTab   = 9
+	keyEnter = 13
+	keyUp    = 38
+	keyDown  = 40
+)
+
+var (
+	currentName   string
+	names         []string
+	filteredNames []string
+)
+
+// The returned value is a bool deciding to prevent the event from propagating
+func processMessageKey(this js.Value, v []js.Value) interface{} {
+	if len(filteredNames) == 0 || currentName == "" {
+		return false
+	}
+
+	startIdx := v[0].Get("target").Get("selectionStart").Int()
+	keyCode := v[0].Get("keyCode").Int()
+	switch keyCode {
+	case keyUp, keyDown:
+		newidx := 0
+		for i, n := range filteredNames {
+			if n == currentName {
+				newidx = i
+				if keyCode == keyDown {
+					newidx = i + 1
+					if newidx == len(filteredNames) {
+						newidx--
+					}
+				} else if keyCode == keyUp {
+					newidx = i - 1
+					if newidx < 0 {
+						newidx = 0
+					}
+				}
+				break
+			}
+		}
+		currentName = filteredNames[newidx]
+	case keyTab, keyEnter:
+		msg := js.Get("msg")
+		val := msg.Get("value").String()
+		newval := val[:startIdx]
+
+		if i := strings.LastIndex(newval, "@"); i != -1 {
+			newval = newval[:i+1] + currentName
+		}
+
+		endVal := val[startIdx:]
+		if len(val) == startIdx || val[startIdx:][0] != ' ' {
+			// insert a space into val so selection indexing can be one line
+			endVal = " " + endVal
+		}
+		msg.Set("value", newval+endVal)
+		msg.Set("selectionStart", len(newval)+1)
+		msg.Set("selectionEnd", len(newval)+1)
+
+		// Clear out filtered names since it is no longer needed
+		filteredNames = nil
+	default:
+		// We only want to handle the caught keys, so return early
+		return false
+	}
+
+	updateSuggestionDiv()
+	return true
+}
+
+func processMessage(v []js.Value) {
+	msg := js.Get("msg")
+	text := strings.ToLower(msg.Get("value").String())
+	startIdx := msg.Get("selectionStart").Int()
+
+	filteredNames = nil
+	if len(text) != 0 {
+		if len(names) > 0 {
+			var caretIdx int
+			textParts := strings.Split(text, " ")
+
+			for i, word := range textParts {
+				// Increase caret index at beginning if not first word to account for spaces
+				if i != 0 {
+					caretIdx++
+				}
+
+				// It is possible to have a double space "  ", which will lead to an
+				// empty string element in the slice. Also check that the index of the
+				// cursor is between the start of the word and the end
+				if len(word) > 0 && word[0] == '@' &&
+					caretIdx <= startIdx && startIdx <= caretIdx+len(word) {
+					// fill filtered first so the "modifier" keys can modify it
+					for _, n := range names {
+						if len(word) == 1 || strings.HasPrefix(strings.ToLower(n), word[1:]) {
+							filteredNames = append(filteredNames, n)
+						}
+					}
+				}
+
+				if len(filteredNames) > 0 {
+					currentName = ""
+					break
+				}
+
+				caretIdx += len(word)
+			}
+		}
+	}
+
+	updateSuggestionDiv()
+}
+
+func updateSuggestionDiv() {
+	const selectedClass = ` class="selectedName"`
+
+	var divs []string
+	if len(filteredNames) > 0 {
+		// set current name to first if not set already
+		if currentName == "" {
+			currentName = filteredNames[0]
+		}
+
+		var hasCurrentName bool
+		divs = make([]string, len(filteredNames))
+
+		// Create inner body of html
+		for i := range filteredNames {
+			divs[i] = "<div"
+			if filteredNames[i] == currentName {
+				hasCurrentName = true
+				divs[i] += selectedClass
+			}
+			divs[i] += ">" + filteredNames[i] + "</div>"
+		}
+
+		if !hasCurrentName {
+			divs[0] = divs[0][:4] + selectedClass + divs[0][4:]
+		}
+	}
+	// The \n is so it's easier to read th source in web browsers for the dev
+	js.Get("suggestions").Set("innerHTML", strings.Join(divs, "\n"))
+}
