9c559c38180bd97518f07aba1fdb47b340d4890f
Zorchenhimer <zorchenhimer@gmail.com>
2019-03-31 00:07:19 -0400

    Revert "Merge branch 'server-shutdown'"
    This reverts commit 13d74faf01986ecf6d8a35e564bcb24f72fac72d, reversing
    changes made to ec0c6752bc236ada82d4c6d4bb957b6f7b7f8f75.
    
    These changes completely broke the RTMP stream.  Reverting until I
    figure out why.

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/nareix/joy4/format"
 	"github.com/nareix/joy4/format/rtmp"
@@ -15,10 +13,9 @@ import (
 )
 
 var (
-	addr       string
-	sKey       string
-	stats      = newStreamStats()
-	chatServer *http.Server
+	addr  string
+	sKey  string
+	stats = newStreamStats()
 )
 
 func setupSettings() error {
@@ -51,6 +48,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	exit := make(chan bool)
+	go handleInterrupt(exit)
+
 	// Load emotes before starting server.
 	var err error
 	if chat, err = newChatRoom(); err != nil {
@@ -72,87 +72,53 @@ func main() {
 	common.LogInfoln("Admin password: ", settings.AdminPassword)
 	common.LogInfoln("Listen and serve ", addr)
 
+	go startServer()
+	go startRmtpServer()
+
+	<-exit
+}
+
+func startRmtpServer() {
 	server := &rtmp.Server{
 		HandlePlay:    handlePlay,
 		HandlePublish: handlePublish,
 	}
-
-	// Define this here so we can set some timeouts and things.
-	chatServer := &http.Server{
-		Addr:           addr,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+	err := server.ListenAndServe()
+	if err != nil {
+		// If the server cannot start, don't pretend we can continue.
+		panic("Error trying to start rtmp server: " + err.Error())
 	}
+}
 
-	chatServer.RegisterOnShutdown(func() { chat.Shutdown() })
-	// rtmp.Server does not implement .RegisterOnShutdown()
-	//server.RegisterOnShutdown(func() { common.LogDebugln("server shutdown callback called.") })
+func startServer() {
+	// Chat websocket
+	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/static/js/", wsStaticFiles)
+	http.HandleFunc("/static/css/", wsStaticFiles)
+	http.HandleFunc("/static/img/", wsImages)
+	http.HandleFunc("/static/main.wasm", wsWasmFile)
+	http.HandleFunc("/emotes/", wsEmotes)
+	http.HandleFunc("/favicon.ico", wsStaticFiles)
+	http.HandleFunc("/chat", handleIndexTemplate)
+	http.HandleFunc("/video", handleIndexTemplate)
+	http.HandleFunc("/help", handleHelpTemplate)
 
-	// These have been moved back to annon functitons so I could use
-	// `server`, `chatServer`, and `exit` in them without needing to
-	// pass them as parameters.
+	http.HandleFunc("/", handleDefault)
 
-	// Signal handler
-	exit := make(chan bool)
-	go func() {
-		ch := make(chan os.Signal)
-		signal.Notify(ch, os.Interrupt)
-		<-ch
-		common.LogInfoln("Closing server")
-		if settings.StreamStats {
-			stats.Print()
-		}
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		// If the server cannot start, don't pretend we can continue.
+		panic("Error trying to start chat/http server: " + err.Error())
+	}
+}
 
-		if err := chatServer.Shutdown(context.Background()); err != nil {
-			common.LogErrorf("Error shutting down chat server: %v", err)
-		}
-
-		common.LogInfoln("Shutdown() sent.  Sending exit.")
-		exit <- true
-	}()
-
-	// Chat and HTTP server
-	go func() {
-		// Use a ServeMux here instead of the default, global,
-		// http handler.  It's a good idea when we're starting more
-		// than one server.
-		mux := http.NewServeMux()
-		mux.HandleFunc("/ws", wsHandler)
-		mux.HandleFunc("/static/js/", wsStaticFiles)
-		mux.HandleFunc("/static/css/", wsStaticFiles)
-		mux.HandleFunc("/static/img/", wsImages)
-		mux.HandleFunc("/static/main.wasm", wsWasmFile)
-		mux.HandleFunc("/emotes/", wsEmotes)
-		mux.HandleFunc("/favicon.ico", wsStaticFiles)
-		mux.HandleFunc("/chat", handleIndexTemplate)
-		mux.HandleFunc("/video", handleIndexTemplate)
-		mux.HandleFunc("/help", handleHelpTemplate)
-
-		mux.HandleFunc("/", handleDefault)
-
-		chatServer.Handler = mux
-		err := chatServer.ListenAndServe()
-		// http.ErrServerClosed is returned when server.Shuddown()
-		// is called.
-		if err != http.ErrServerClosed {
-			// If the server cannot start, don't pretend we can continue.
-			panic("Error trying to start chat/http server: " + err.Error())
-		}
-		common.LogDebugln("ChatServer closed.")
-	}()
-
-	// RTMP server
-	go func() {
-		err := server.ListenAndServe()
-		// http.ErrServerClosed is returned when server.Shuddown()
-		// is called.
-		if err != http.ErrServerClosed {
-			// If the server cannot start, don't pretend we can continue.
-			panic("Error trying to start rtmp server: " + err.Error())
-		}
-		common.LogDebugln("RTMP server closed.")
-	}()
-
-	<-exit
+func handleInterrupt(exit chan bool) {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	common.LogInfoln("Closing server")
+	if settings.StreamStats {
+		stats.Print()
+	}
+	exit <- true
 }
