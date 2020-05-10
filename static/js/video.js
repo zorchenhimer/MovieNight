95c06636841d658fbdc1d539ec09f1841d9519aa
Akshay Shekher <akshay.shkekher@gmail.com>
2020-05-10 10:51:34 -0700

    Made video muted by default and added overlay to ask the user to unmute

--- static/js/video.js
+++ static/js/video.js
@@ -2,16 +2,25 @@
 
 
 function initPlayer() {
-    if (flvjs.isSupported()) {
-        var videoElement = document.getElementById("videoElement");
-        var flvPlayer = flvjs.createPlayer({
-            type: "flv",
-            url: "/live"
-        });
-        flvPlayer.attachMediaElement(videoElement);
-        flvPlayer.load();
-        flvPlayer.play();
+    if (!flvjs.isSupported()) {
+        console.warn('flvjs not supported');
+        return;
     }
+
+    let videoElement = document.querySelector("#videoElement");
+    let flvPlayer = flvjs.createPlayer({
+        type: "flv",
+        url: "/live"
+    });
+    flvPlayer.attachMediaElement(videoElement);
+    flvPlayer.load();
+    flvPlayer.play();
+
+    let overlay = document.querySelector('#videoOverlay');
+    overlay.onclick = () => {
+        overlay.style.display = 'none';
+        videoElement.muted = false;
+    };
 }
 
 window.addEventListener("load", initPlayer);
