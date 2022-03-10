3230647e3e8d7ed0326e9812a3f529a19f2a7f9c
joeyak <joeyak97@gmail.com>
2022-03-09 20:51:56 -0500

    Add tests for /me and /nick

--- _uitests/main_test.go
+++ _uitests/main_test.go
@@ -16,6 +16,7 @@ const (
 	sendInput        = `//input[@id="send"]`
 	nameSpan         = `//span[contains(@class, "name") and text()="%s"]`
 	msgSpan          = `//span[contains(@class, "msg") and text()="%s"]`
+	meCmdSpan        = `//span[contains(@class, "cmdme") and text()="%s"]`
 )
 
 func openBrowser() (playwright.Browser, error) {
@@ -71,7 +72,21 @@ func openChat(t *testing.T, browser playwright.Browser, username string) (playwr
 	return page, nil
 }
 
-func TestAccessFirefox(t *testing.T) {
+func sendMessage(page playwright.Page, msg string) error {
+	err := page.Type(msgInputTextarea, msg)
+	if err != nil {
+		return fmt.Errorf("could not find msg box: %w", err)
+	}
+
+	err = page.Click(sendInput)
+	if err != nil {
+		return fmt.Errorf("could not send message: %w", err)
+	}
+
+	return nil
+}
+
+func TestAccess(t *testing.T) {
 	browser, err := openBrowser()
 	if err != nil {
 		t.Error(err)
@@ -84,7 +99,9 @@ func TestAccessFirefox(t *testing.T) {
 	}
 }
 
-func TestSeeingChat(t *testing.T) {
+func TestChatMessage(t *testing.T) {
+	const msg = "testing 1 2 3"
+
 	browser, err := openBrowser()
 	if err != nil {
 		t.Error(err)
@@ -101,19 +118,63 @@ func TestSeeingChat(t *testing.T) {
 		t.Errorf("could not open chat for user 2: %v", err)
 	}
 
-	msg := "testing 1 2 3"
-	err = page1.Type(msgInputTextarea, msg)
+	err = sendMessage(page1, msg)
 	if err != nil {
-		t.Errorf("could not find msg box: %v", err)
-	}
-
-	err = page1.Click(sendInput)
-	if err != nil {
-		t.Errorf("could not send message: %v", err)
+		t.Error(err)
 	}
 
 	_, err = page2.WaitForSelector(fmt.Sprintf(msgSpan, msg))
 	if err != nil {
 		t.Errorf("did not find testing msg: %v", err)
+	}
+}
+
+func TestCommandNick(t *testing.T) {
+	const nick = "newNick"
+
+	browser, err := openBrowser()
+	if err != nil {
+		t.Error(err)
+	}
+	defer browser.Close()
+
+	page, err := openChat(t, browser, "testUser")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = sendMessage(page, fmt.Sprintf("/nick %s", nick))
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = page.WaitForSelector(fmt.Sprintf(nameSpan, nick))
+	if err != nil {
+		t.Errorf("could not find nick change message: %v", err)
+	}
+}
+
+func TestCommandMe(t *testing.T) {
+	const msg = "this is a me message"
+
+	browser, err := openBrowser()
+	if err != nil {
+		t.Error(err)
+	}
+	defer browser.Close()
+
+	page, err := openChat(t, browser, "testUser")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = sendMessage(page, fmt.Sprintf("/me %s", msg))
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = page.WaitForSelector(fmt.Sprintf(meCmdSpan, msg))
+	if err != nil {
+		t.Errorf("could not find user me message: %v", err)
 	}
 }
