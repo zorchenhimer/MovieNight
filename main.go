9298f606f1ac74da6d08d7cd77572b5679d4cf5b
joeyak <joeyak97@gmail.com>
2022-03-22 21:06:37 -0400

    Handle emotes dir and remove debug command

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"os"
 	"os/signal"
 	"path"
-	"strings"
 	"time"
 
 	"github.com/alexflint/go-arg"
@@ -51,50 +50,49 @@ func setupSettings(adminPass string, confFile string) error {
 //go:embed static/*.html static/css static/img static/js
 var staticFs embed.FS
 
-func writeStaticFiles(fileDir, name string) error {
+func writeStaticFiles(name string) error {
 	items, err := staticFs.ReadDir(name)
 	if err != nil {
 		return fmt.Errorf("could not read staticFs directory %#v: %w", name, err)
 	}
 
 	for _, item := range items {
-		fsPath := path.Join(name, item.Name())
-		filePath := strings.Replace(fsPath, "static", fileDir, 1)
+		path := path.Join(name, item.Name())
 
-		_, err := os.Open(filePath)
+		_, err := os.Open(path)
 		notExist := errors.Is(err, os.ErrNotExist)
 
 		if item.IsDir() {
 			if notExist {
-				fmt.Printf("creating dir %q\n", filePath)
+				common.LogInfof("creating dir %q\n", path)
 
-				err = os.MkdirAll(filePath, os.ModeDir)
+				err = os.MkdirAll(path, os.ModeDir)
 				if err != nil {
 					return fmt.Errorf("could not make missing directory: %w", err)
 				}
 			}
 
-			err = writeStaticFiles(fileDir, fsPath)
+			err = writeStaticFiles(path)
 			if err != nil {
 				return err
 			}
 		} else if notExist {
-			fmt.Printf("creating file %q\n", filePath)
+			common.LogInfof("creating file %q\n", path)
 
-			staticFile, err := staticFs.Open(fsPath)
+			file, err := staticFs.Open(path)
 			if err != nil {
-				return fmt.Errorf("could not open embeded file %q: %w", fsPath, err)
+				return fmt.Errorf("could not open embeded file %q: %w", path, err)
 			}
 
 			var staticData []byte
-			_, err = staticFile.Read(staticData)
+			_, err = file.Read(staticData)
 			if err != nil {
-				return fmt.Errorf("could not read embeded file %q: %w", fsPath, err)
+				return fmt.Errorf("could not read embeded file %q: %w", path, err)
 			}
 
-			err = os.WriteFile(filePath, staticData, 0644)
+			err = os.WriteFile(path, staticData, 0644)
 			if err != nil {
-				return fmt.Errorf("could not write static data to file %q: %w", filePath, err)
+				return fmt.Errorf("could not write static data to file %q: %w", path, err)
 			}
 		}
 	}
@@ -110,7 +108,6 @@ func main() {
 		AdminPass  string `arg:"-a,--admin" help:"Set admin password.  Overrides configuration in settings.json.  This will not write the password to settings.json."`
 		PullEmotes bool   `arg:"-e,--pull-emotes" help:"Pull emotes"`
 		ConfigFile string `arg:"-f,--config" default:"./settings.json" help:"URI of the conf file"`
-		StaticDir  string `arg:"-s,--static" default:"static" help:"The directory MovieNight looks for the static dir"`
 	}
 	arg.MustParse(&args)
 
@@ -120,10 +117,19 @@ func main() {
 		log.Fatalf("Error loading settings: %v\n", err)
 	}
 
-	err = writeStaticFiles(args.StaticDir, ".")
+	err = writeStaticFiles(".")
 	if err != nil {
 		common.LogErrorf("Error writing static files: %v\n", err)
 		os.Exit(1)
+	}
+
+	const staticEmotesDir = "static/emotes"
+	if _, err := os.Open(staticEmotesDir); errors.Is(err, os.ErrNotExist) {
+		common.LogInfof("creating dir %q\n", staticEmotesDir)
+		if err := os.MkdirAll(staticEmotesDir, os.ModeDir); err != nil {
+			common.LogErrorf("Error creating %q: %v\n", staticEmotesDir, err)
+			os.Exit(1)
+		}
 	}
 
 	if args.PullEmotes {
